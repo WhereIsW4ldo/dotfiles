ca6098eb738022a32df6895180d79ffe53ccea97
Waldo Wautelet <Waldo.Wautelet@ae.be>
2025-06-05 17:56:54 +0200

    feat: neo-tree reveal without focus!

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,52 +1,55 @@
 return {
-  {
-    "antosha417/nvim-lsp-file-operations",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-neo-tree/neo-tree.nvim",
-    },
-    config = function()
-      require("lsp-file-operations").setup()
-    end,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    name = "neo-tree",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    lazy = false,
-    config = function()
-      require("neo-tree").setup({
-        window = {
-          mappings = {
-            ["P"] = { "toggle_preview", config = { use_float = true } },
-            ["l"] = "focus_preview",
-            ["<C-k>"] = { "scroll_preview", config = { direction = 10 } },
-            ["<C-j>"] = { "scroll_preview", config = { direction = -10 } },
-          },
-        },
-        default_component_configs = {
-          filesystem = {
-            follow_current_file = {
-              enabled = true,
-              leave_dirs_open = true,
-            },
-          },
-        },
-        buffers = {
-          follow_current_file = {
-            enabled = true,
-            leave_dirs_open = true,
-          },
-        },
-      })
+	{
+		"antosha417/nvim-lsp-file-operations",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-neo-tree/neo-tree.nvim",
+		},
+		config = function()
+			require("lsp-file-operations").setup()
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		name = "neo-tree",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		lazy = false,
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				enable_git_status = true,
+				enable_diagnostics = true,
+				window = {
+					mappings = {
+						["P"] = { "toggle_preview", config = { use_float = true } },
+						["l"] = "focus_preview",
+						["<C-k>"] = { "scroll_preview", config = { direction = 10 } },
+						["<C-j>"] = { "scroll_preview", config = { direction = -10 } },
+					},
+				},
+				default_component_configs = {
+					filesystem = {
+						follow_current_file = {
+							enabled = true,
+							leave_dirs_open = true,
+						},
+					},
+				},
+				buffers = {
+					follow_current_file = {
+						enabled = true,
+						leave_dirs_open = true,
+					},
+				},
+			})
 
-      vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
-      vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float <CR>", {})
-      vim.keymap.set("n", "<leader>sc", ":Neotree reveal<CR>", {})
-    end,
-  },
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
+			vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float <CR>", {})
+			vim.keymap.set("n", "<leader>sc", ":Neotree show reveal<CR>", {})
+		end,
+	},
 }
