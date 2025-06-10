de964660ab03c0a6b784097f5b15c9dd0e441cf5
Waldo Wautelet <Waldo.Wautelet@ae.be>
2025-06-10 20:19:14 +0200

    feat: more configs

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,24 @@
-return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+-- return {
+--   "nvimtools/none-ls.nvim",
+--   dependencies = {
+--     "nvimtools/none-ls-extras.nvim",
+--   },
+--   config = function()
+--     local null_ls = require("null-ls")
+--
+--     null_ls.setup({
+--       sources = {
+--         null_ls.builtins.formatting.stylua,
+--         null_ls.builtins.formatting.prettier,
+--
+--         require("none-ls.diagnostics.eslint_d"),
+--       },
+--     })
+--
+--     vim.diagnostic.config({ virtual_text = true })
+--
+--     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+--   end,
+-- }
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-
-        require("none-ls.diagnostics.eslint_d"),
-      },
-    })
-
-    vim.diagnostic.config({ virtual_text = true })
-
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
-}
+return {}
