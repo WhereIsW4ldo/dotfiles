56ca3deb6586862b030a6555435264ff05b67c4c
waldo wautelet <waldowautelet4@gmail.com>
2025-06-11 21:20:52 +0200

    feat: update none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,39 @@
--- return {
---   "nvimtools/none-ls.nvim",
---   dependencies = {
---     "nvimtools/none-ls-extras.nvim",
---   },
---   config = function()
---     local null_ls = require("null-ls")
---
---     null_ls.setup({
---       sources = {
---         null_ls.builtins.formatting.stylua,
---         null_ls.builtins.formatting.prettier,
---
---         require("none-ls.diagnostics.eslint_d"),
---       },
---     })
---
---     vim.diagnostic.config({ virtual_text = true })
---
---     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
---   end,
--- }
+return {
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+			"ThePrimeagen/refactoring.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
 
-return {}
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+
+					require("none-ls.diagnostics.eslint_d"),
+					null_ls.builtins.diagnostics.mypy,
+					null_ls.builtins.diagnostics.terraform_validate,
+					null_ls.builtins.diagnostics.todo_comments,
+
+					null_ls.builtins.code_actions.refactoring,
+
+					null_ls.builtins.completion.luasnip,
+				},
+			})
+
+			vim.diagnostic.config({ virtual_text = true })
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.4", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+	},
+}
