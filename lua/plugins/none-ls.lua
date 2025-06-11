4abc093ec8b320c6c988fc3eaede647af0472a41
waldo wautelet <waldowautelet4@gmail.com>
2025-06-11 21:40:57 +0200

    feat: further none-ls config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,39 +1,34 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = {
-			"nvimtools/none-ls-extras.nvim",
-			"ThePrimeagen/refactoring.nvim",
-		},
-		config = function()
-			local null_ls = require("null-ls")
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+      "ThePrimeagen/refactoring.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
 
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.csharpier,
+          null_ls.builtins.formatting.terraform_fmt,
 
-					require("none-ls.diagnostics.eslint_d"),
-					null_ls.builtins.diagnostics.mypy,
-					null_ls.builtins.diagnostics.terraform_validate,
-					null_ls.builtins.diagnostics.todo_comments,
+          require("none-ls.diagnostics.eslint_d"),
+          null_ls.builtins.diagnostics.mypy,
+          null_ls.builtins.diagnostics.terraform_validate,
+          null_ls.builtins.diagnostics.todo_comments,
 
-					null_ls.builtins.code_actions.refactoring,
+          null_ls.builtins.code_actions.refactoring,
 
-					null_ls.builtins.completion.luasnip,
-				},
-			})
+          null_ls.builtins.completion.luasnip,
+        },
+      })
 
-			vim.diagnostic.config({ virtual_text = true })
+      vim.diagnostic.config({ virtual_text = true })
 
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		end,
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		-- follow latest release.
-		version = "v2.4", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		-- install jsregexp (optional!).
-		build = "make install_jsregexp",
-	},
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
