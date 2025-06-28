7f8058d04d06f508e4c7d01e45d8e6a843842688
waldo wautelet <waldowautelet4@gmail.com>
2025-06-28 15:26:29 +0200

    feat: more lsp stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,65 +17,58 @@ return {
     "mason-org/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-        ensure_installed = {
-          "lua_ls",
-          "csharp_ls",
-          "jsonls",
-          -- "pylsp",
-          "rust_analyzer",
-          "svelte",
-          "ts_ls",
-          "terraformls",
-        },
-        auto_install = true,
+      ensure_installed = {
+        "lua_ls",
+        "csharp_ls",
+        "jsonls",
+        -- "pylsp",
+        "rust_analyzer",
+        "svelte",
+        "ts_ls",
+        "terraformls",
+      },
+      auto_install = true,
     },
   },
   {
     "neovim/nvim-lspconfig",
   },
-    {
-        'kosayoda/nvim-lightbulb',
-        opts = {
-            autocmd = { enabled = true }
-        }
+  {
+    "onsails/lspkind.nvim",
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    opts = {
+      bind = true,
+      handler_opts = {
+        border = "rounded",
+      },
     },
-    {
-        'onsails/lspkind.nvim',
-    },
-    {
-        'ray-x/lsp_signature.nvim',
-        opts = {
-            bind = true,
-            handler_opts = {
-                border = "rounded"
-            }
-        }
-    },
-    {
-        'smjonas/inc-rename.nvim',
-    },
-    {
-        'jubnzv/virtual-types.nvim',
-    },
-    {
-        'aznhe21/actions-preview.nvim',
-    },
-    {
-        'VidocqH/lsp-lens.nvim',
-    },
-    {
-        'hinell/lsp-timeout.nvim',
-        dependencies = { 'neovim/nvim-lspconfig' }
-    },
-    {
-        'esmuellert/nvim-eslint'
-    },
-    {
-        'sontungexpt/better-diagnostic-virtual-text',
-        event = 'LspAttach',
-        config = function(opts)
-            require('better-diagnostic-virtual-text').setup(opts)
-        end
-    },
-
+  },
+  {
+    "smjonas/inc-rename.nvim",
+  },
+  {
+    "jubnzv/virtual-types.nvim",
+  },
+  {
+    "aznhe21/actions-preview.nvim",
+  },
+  {
+    "VidocqH/lsp-lens.nvim",
+  },
+  {
+    "hinell/lsp-timeout.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+  },
+  {
+    "esmuellert/nvim-eslint",
+  },
+  {
+    "sontungexpt/better-diagnostic-virtual-text",
+    event = "LspAttach",
+    config = function(opts)
+      require("better-diagnostic-virtual-text").setup(opts)
+    end,
+  },
 }
